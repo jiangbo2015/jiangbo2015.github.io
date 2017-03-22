44949e6bf2cd81b3d8cea32d4abb13ec99c2aa98
jiangbo <772051431@qq.com>
2017-03-22 10:19:45 +0800

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import DataTable from './DataTable'
+import DropOption from './DropOption'
+import Iconfont from './Iconfont'
+import Search from './Search'
+import * as Layout from './Layout/index.js'
+
+import layer from './layer'
+
+export { Layout, DataTable, DropOption, Iconfont, Search, layer }
